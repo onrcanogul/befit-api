811c6fd591eb472e28bee99ca33d53be41fa1fbc
Onurcan Oğul <147406204+onrcanogul@users.noreply.github.com>
2024-09-28 23:21:38 +0300

    Test Pipeline

--- Core/BeFit.Application/Mapping/Mapping.cs
+++ Core/BeFit.Application/Mapping/Mapping.cs
@@ -16,7 +16,7 @@ namespace BeFit.Application.Mapper
         public Mapping()
         { 
             CreateMap<Nutrient, NutrientDto>();
-            VVCC
+            VV
             CreateMap<BaseEntity, BaseDto>();
             CreateMap<Food, FoodDto>().IncludeBase<Nutrient, NutrientDto>().ReverseMap();
             CreateMap<Drink, DrinkDto>().IncludeBase<Nutrient, NutrientDto>().ReverseMap();
